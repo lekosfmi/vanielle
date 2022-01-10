b4bb17db855d949cbc8d229cf29fdef7e37ffd50
Vanielle Lee <vanielle@odecloud.com>
2022-01-10 08:53:26 -0800

    Update personalisation

--- pages/_document.js
+++ pages/_document.js
@@ -8,10 +8,10 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
+      title: "Vanielle LEE",
+      description: 'My personal blog where I talk and discuss various topics based on my interests.',
       image:
-        'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+        'https://avatars.githubusercontent.com/u/10788981?v=4'
     }
 
     return (
@@ -22,12 +22,7 @@ class MyDocument extends Document {
           <meta property="og:site_name" content={meta.title} />
           <meta property="og:description" content={meta.description} />
           <meta property="og:title" content={meta.title} />
-          <meta property="og:image" content={meta.image} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@yourname" />
-          <meta name="twitter:title" content={meta.title} />
-          <meta name="twitter:description" content={meta.description} />
-          <meta name="twitter:image" content={meta.image} />
+          <meta property="og:image" content={meta.image} />>
         </Head>
         <body>
           <Main />
